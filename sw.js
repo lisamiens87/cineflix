53cad4f4786fec20652bc2083d6c4d6e45159aaa
lisamiens87 <alexandre.mesnier@gmail.com>
2026-07-30 00:06:05 +0200

    Build 2907t

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    afficherait « déjà sur le serveur » pour un titre qui n'y est plus.
 
    VERSION : suivre le BUILD d'index.html. Changer les deux ensemble. */
-const BUILD = '2907s';
+const BUILD = '2907t';
 const CACHE = 'cineflix-' + BUILD;
 const SHELL = ['./', './index.html', './manifest.json', './icon.svg',
                './app.css', './config.js', './app-01-noyau.js', './app-02-outils.js',
